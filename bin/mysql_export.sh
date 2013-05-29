3c02fd7f313bc4729b10be54dcc67adc32049d4e
Anandan <andy.compeer@gmail.com>
2013-05-29 09:03:02 -0400

    A mysql export bash script to run sqoop-like MysqlExport job

---
+++ bin/mysql_export.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+cd ../
+mvn package || exit
+## copy over gHadoop.jar 
+cp lib/gHadoop.jar ~/lib/
+
+for i in /usr/lib/hive/lib/*.jar
+do
+   HADOOP_CLASSPATH=$HADOOP_CLASSPATH:$i
+done
+libjars=""
+for i in ~/lib/*.jar
+do
+   HADOOP_CLASSPATH=$HADOOP_CLASSPATH:$i
+   libjars="$i,$libjars"
+done
+libjars=$(echo $libjars | sed 's/,$//')
+
+sudo -uhdfs sh -c "
+export HADOOP_CLASSPATH=$HADOOP_CLASSPATH
+hadoop jar lib/gHadoop.jar com.grooveshark.hadoop.jobs.MysqlExport -libjars $libjars -myjson dsn.json
+"
