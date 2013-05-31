540223a95da715180c66394decbafb6376961e92
Anandan <andy.compeer@gmail.com>
2013-05-31 16:27:25 -0400

    A shell script to run explode unique job

---
+++ bin/explode_unique.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+mvn package || exit
+## copy over gHadoop.jar 
+cp lib/gHadoop.jar ~/lib/
+
+for i in /usr/lib/hive/lib/*.jar
+do
+   HADOOP_CLASSPATH=$HADOOP_CLASSPATH:$i
+done
+libjars=""
+for i in ~/lib/*.jar
+do
+   HADOOP_CLASSPATH=$HADOOP_CLASSPATH:$i
+   libjars="$i,$libjars"
+done
+libjars=$(echo $libjars | sed 's/,$//')
+
+export HADOOP_CLASSPATH=$HADOOP_CLASSPATH
+hadoop jar lib/gHadoop.jar com.grooveshark.hadoop.jobs.ExplodeUnique -libjars $libjars -myjson dsn.json
+#sudo -uhdfs sh -c "
+#export HADOOP_CLASSPATH=$HADOOP_CLASSPATH
+#hadoop jar lib/gHadoop.jar com.grooveshark.hadoop.jobs.ExplodeUnique -libjars $libjars -myjson dsn.json
+#"
