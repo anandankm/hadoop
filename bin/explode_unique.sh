dfcced0492702113341509795339c882f0c9cbd7
Anandan <andy.compeer@gmail.com>
2013-06-12 15:29:03 -0400

    Pass partition values as java arguments to the hadoop job

--- bin/explode_unique.sh
+++ bin/explode_unique.sh
@@ -16,8 +16,8 @@ done
 libjars=$(echo $libjars | sed 's/,$//')
 
 export HADOOP_CLASSPATH=$HADOOP_CLASSPATH
-hadoop jar lib/gHadoop.jar com.grooveshark.hadoop.jobs.ExplodeUnique -libjars $libjars -myjson dsn.json
+hadoop jar lib/gHadoop.jar com.grooveshark.hadoop.jobs.ExplodeUnique -libjars $libjars --myjson dsn.json.copy --partitionValues sessionid,2012:2013,2012-01-01:2013-06-09
 #sudo -uhdfs sh -c "
 #export HADOOP_CLASSPATH=$HADOOP_CLASSPATH
-#hadoop jar lib/gHadoop.jar com.grooveshark.hadoop.jobs.ExplodeUnique -libjars $libjars -myjson dsn.json
+#hadoop jar lib/gHadoop.jar com.grooveshark.hadoop.jobs.ExplodeUnique -libjars $libjars -myjson dsn.json -run Sequence
 #"
