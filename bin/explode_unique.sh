952186d1c01f55b72e74aaf8bf708f26ab258a66
Anandan <andy.compeer@gmail.com>
2013-07-23 14:50:40 -0400

    Use properties from bash for the hadoop job

--- bin/explode_unique.sh
+++ bin/explode_unique.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-mvn package || exit
-## copy over gHadoop.jar 
-cp lib/gHadoop.jar ~/lib/
+#mvn package || exit
+## copy over gHadoop.jar
+jobJar="lib/gHadoop.jar"
+cp $jobJar ~/lib/
 
 for i in /usr/lib/hive/lib/*.jar
 do
@@ -16,7 +17,21 @@ done
 libjars=$(echo $libjars | sed 's/,$//')
 
 export HADOOP_CLASSPATH=$HADOOP_CLASSPATH
-hadoop jar lib/gHadoop.jar com.grooveshark.hadoop.jobs.ExplodeUnique -libjars $libjars --myjson dsn.json.copy --partitionValues sessionid,2012:2013,2012-01-01:2013-06-09
+hivePrefix="/user/anandan.rangasamy"
+hiveTable="ar_uniqs_daily_artists"
+partitionColumns="uniq_type,year,datecomputed"
+partitionValues="sessionid,2012:2013,2012-01-01:2013-06-11"
+outputPath="/user/anandan.rangasamy/tmp/artists_uniqs_daily"
+echo "Running hadoop jar"
+
+hadoop jar $jobJar com.grooveshark.hadoop.jobs.ExplodeUnique \
+    -libjars $libjars \
+    --jobJar $jobJar \
+    --partitionValues $partitionValues \
+    --hivePrefix $hivePrefix \
+    --hiveTable $hiveTable \
+    --partitionColumns  $partitionColumns \
+    --outputPath $outputPath
 #sudo -uhdfs sh -c "
 #export HADOOP_CLASSPATH=$HADOOP_CLASSPATH
 #hadoop jar lib/gHadoop.jar com.grooveshark.hadoop.jobs.ExplodeUnique -libjars $libjars -myjson dsn.json -run Sequence
